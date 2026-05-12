62db7dde08ce422c8a044c86691ab886a26182cd
Danilo Fernandes de Sousa <fernandesdanilo2026@gmail.com>
2026-05-12 19:27:48 -0300

    Implementação Scrapy da CNN

--- scrapers/executarSpiders.py
+++ scrapers/executarSpiders.py
@@ -1,6 +1,11 @@
 from multiprocessing import Process
 from spiders.g1Spider import g1_run_spider
+from spiders.cnnspider import cnn_run_spider
 
 g1_scrapy = Process(target = g1_run_spider)
 g1_scrapy.start()
 g1_scrapy.join()
+
+cnn_scrapy = Process(target= cnn_run_spider)
+cnn_scrapy.start()
+cnn_scrapy.join()
---
+++ scrapers/spiders/cnnspider.py
@@ -0,0 +1,61 @@
+import scrapy
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+from datetime import datetime
+
+class cnn_spider(scrapy.Spider):
+    name = 'CNN'
+    data = str(datetime.now().strftime("%Y-%m-%d"))
+    links = []
+    i = 1
+
+    def start_requests(self):
+        yield scrapy.Request('https://www.cnnbrasil.com.br/tudo-sobre/feminicidio/')
+
+    def parse(self, response, **kwargs):
+        for news in response.css('ul figure'):
+            if (self.data in news.css('time').attrib['datetime']):
+                yield response.follow(news.css('a').attrib['href'], self.parse_pegar)
+
+        pag = response.css('a:contains("Pr")::attr(href)').get() 
+
+        if pag:
+            yield response.follow(pag, self.parse)
+        else:
+            yield response.follow(self.links[0], self.parse_pegar)
+
+    def parse_pegar(self, response):
+        alltext = response.css('.text-lg p::text').getall()
+        news = ''
+        for text in alltext:
+             news+= text
+
+        yield {
+                'portal': 'CNN',
+                'title': response.css('article').css('header').css('h1::text').get(),
+                'data': response.css('article').css('span').css('.timestamp__date::text').getall(),
+                'links': response.css('ul figure a').attrib['href'],
+                'texto':  news,
+                'autor': response.css('article').css('header').css('span').css('a').css('span::text').get()
+        }
+        for self.i in range(len(self.links)):
+                get = self.links[self.i]
+                self.i += 1
+                yield response.follow(get, self.parse_pegar)
+
+              
+def cnn_run_spider():
+    settings = get_project_settings()
+    settings.set(
+        'FEEDS',
+        {
+            './resultados/cnn.json': {
+                'format': 'json',
+                'encoding': 'utf-8',
+                'overwrite': True
+            }
+        }
+    )
+    process = CrawlerProcess(settings)
+    process.crawl(cnn_spider)
+    process.start()
