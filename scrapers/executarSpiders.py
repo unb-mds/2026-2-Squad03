05f04c0d50c71b205138302b7d039aaa39c2b890
jadson <jadsonrleandro@gmail.com>
2026-05-13 20:57:52 -0300

    Implementação do Scrapy do metropoles e Remoção do Index

--- scrapers/executarSpiders.py
+++ scrapers/executarSpiders.py
@@ -2,6 +2,7 @@ from multiprocessing import Process
 from spiders.g1Spider import g1_run_spider
 from spiders.cnnspider import cnn_run_spider
 from spiders.r7spider import r7_run_spider
+from spiders.metropoles import metropoles_run_spider
 
 
 g1_scrapy = Process(target = g1_run_spider)
@@ -15,3 +16,7 @@ cnn_scrapy.join()
 r7_scrapy = Process(target= r7_run_spider)
 r7_scrapy.start()
 r7_scrapy.join()
+
+metropoles_scrapy = Process(target= metropoles_run_spider)
+metropoles_scrapy.start()
+metropoles_scrapy.join()
