f6823deb984fc767930eb780600a6f528f87d3cf
OBagomes <vitorbarretogomes1@gmail.com>
2026-05-21 16:44:10 -0300

    feat: configura sistema inicial de rotas

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,63 +1,23 @@
-import "./App.css";
-import StatCard from "./components/StatCard";
-import Sidebar from "./components/Sidebar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Cadastro from "./pages/Cadastro";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  const stats = [
-    {
-      title: "Total de Notícias",
-      value: "0.000",
-      description: "00,00% vs período anterior",
-    },
-    {
-      title: "Média por dia",
-      value: "0.000",
-      description: "00,00% vs período anterior",
-    },
-    {
-      title: "Comparação",
-      value: "00,00%",
-      description: "vs Semana anterior",
-    },
-  ];
-
   return (
-    <div className="app">
-      <Sidebar />
+    <BrowserRouter basename="/2026-2-VeritasIA">
+      <Routes>
+        <Route path="/" element={<Home />} />
 
-      <main className="content">
-        <header className="header">
-          <div>
-            <h2>Dashboard</h2>
-            <p>Visão geral do monitoramento de notícias</p>
-          </div>
+        <Route path="/login" element={<Login />} />
 
-          <div className="header-actions">
-            <button className="date-button">01/05/2024 - 31/05/2024</button>
-            <span className="bell">🔔</span>
+        <Route path="/cadastro" element={<Cadastro />} />
 
-            <div className="user-box">
-              <div className="avatar"></div>
-              <div>
-                <strong>Usuário</strong>
-                <p>Analista</p>
-              </div>
-            </div>
-          </div>
-        </header>
-
-        <section className="cards">
-          {stats.map((stat, index) => (
-            <StatCard
-              key={index}
-              title={stat.title}
-              value={stat.value}
-              description={stat.description}
-            />
-          ))}
-        </section>
-      </main>
-    </div>
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
