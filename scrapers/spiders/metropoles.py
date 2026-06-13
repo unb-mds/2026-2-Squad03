1bd151a2a630c79943ed1864313e0c3a22657228
jadson <jadsonrleandro@gmail.com>
2026-06-13 18:35:35 -0300

    refatoração g1Spider/metropoles Co-Authored: Danilo Fernandes <fernandesdanilo2026@gmail.com>

--- scrapers/spiders/metropoles.py
+++ scrapers/spiders/metropoles.py
@@ -4,6 +4,8 @@ from scrapy.utils.project import get_project_settings
 from playwright.sync_api import sync_playwright
 from datetime import date
 
+from urllib.parse import urljoin
+
 class metropoles_spider(scrapy.Spider):
     name = 'metropoles'
 
@@ -11,17 +13,24 @@ class metropoles_spider(scrapy.Spider):
         self.start_urls = urls or []
 
     def parse(self, response, **kwargs):
-        data_da_publicacao = response.css('time').attrib['datetime']
-        dia_da_publicacao = data_da_publicacao[8:10]
+        data_da_publicacao = response.css(
+            'span.inline-block.tracking-tight.text-neutral-700::text'
+        ).get()
+
+        if not data_da_publicacao:
+            return
+
+        dia_da_publicacao = data_da_publicacao[0:2]
         if(int(dia_da_publicacao) == date.today().day):
 
-            alltext = response.css('article p:not(.data)::text').getall()
+            alltext = response.css('article p *::text, article p::text').getall()
 
             # colocanto toda a noticia em uma unica string
 
-            news = ''
-            for text in alltext:
-                news += text
+            news = ' '.join(
+                t.strip()
+                for t in alltext
+                if t.strip() )
 
             yield { 
                     'portal': 'Metrópoles',
@@ -38,28 +47,55 @@ def play_writght():
 
     with sync_playwright() as pw:
         todas_noticias_do_dia = False
+
         browser = pw.firefox.launch(headless = True)
         page1 = browser.new_page()
         page2 = browser.new_page()
-        page1.goto("https://www.metropoles.com/tag/feminicidio", wait_until='load')
 
-        while todas_noticias_do_dia != True:
-            page1.get_by_role("button", name = "Carregar mais notícias", exact = False).click()
-            newsContent = page1.locator('h4 > a').all()
-            urls = []
-            for news in newsContent:
-                urls.append(news.get_attribute(name="href"))
-            
-            ultima_noticia = urls[len(urls) - 1] # pegando ultima noticia da lista
-            page2.goto(ultima_noticia, wait_until='load')
-            data_da_publicacao = page2.locator('time').nth(0).get_attribute(name = "datetime") # data que a ultima noticia foi publicada
-            mes_da_publicacao = data_da_publicacao[5:7]
-            dia_da_publicacao = data_da_publicacao[8:10]
-            todas_noticias_do_dia = verificar_dia_mes(dia_da_publicacao, mes_da_publicacao)
+        page1.goto(
+            "https://www.metropoles.com/tag/feminicidio",
+            wait_until="load"
+        )
+
+        while not todas_noticias_do_dia:
+
+            page1.get_by_role(
+                "button",
+                name="Ver mais notícias",
+                exact=False
+            ).click()
+
+            page1.wait_for_timeout(2000)
+
+            urls = page1.locator("h3 a[href]").evaluate_all(
+                """
+                els => [...new Set(
+                    els.map(el => el.href)
+                )]
+                """
+            )
+
+            print(urls)
+            ultima_noticia = urls[-1]
+
+            page2.goto(ultima_noticia, wait_until="load")
+
+            texto = page2.locator(
+                "span.inline-block.tracking-tight.text-neutral-700"
+            ).first.text_content()
+
+            data_publicacao = texto.split(",")[0]
+
+            print(data_publicacao)
+
+            dia = data_publicacao[0:2]
+            mes = data_publicacao[3:5]
+
+            todas_noticias_do_dia = verificar_dia_mes(dia, mes)
+
         browser.close()
 
     return urls
-
 def verificar_dia_mes(dia, mes):
     if (int(mes) == date.today().month): # comparando o mes com o mes atual
         if (int(dia) < date.today().day):
@@ -89,3 +125,4 @@ def metropoles_run_spider():
     process = CrawlerProcess(settings)
     process.crawl(metropoles_spider, urls)
     process.start()
+
