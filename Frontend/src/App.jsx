e582595a62608ba36dbb3ffa246d41b4086de109
Daniel Rodrigues Freitas <06daniel.freitas@gmail.com>
2026-06-21 13:00:39 -0300

    feat: cria página Sobre com novo design e ajustes na página de Notícias

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import Noticias from "./pages/Noticias";
+import Sobre from "./pages/Sobre";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route path="/" element={<Dashboard />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/noticias" element={<Noticias />} />
+        <Route path="/sobre" element={<Sobre />} />
       </Routes>
     </HashRouter>
   );
