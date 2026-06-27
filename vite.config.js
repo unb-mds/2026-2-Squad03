ee28b69e5575baa02691cf43362f3240bf30b93f
SchneiderCode1 <Henriquesfrosa@gmail.com>
2026-06-27 02:01:30 -0300

    Corrigindo 2x package.json

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  root: './', // Agora a raiz é a raiz do repositório
   base: "/2026-2-VeritasIA/",
-});
+  plugins: [react()],
+  // ... resto da configuração
+})
